c7affc96968692c46b7c4d07bbab28d3d52d1ac5
josejbecky <josejbeck99@yahoo.com>
2024-10-31 19:19:35 +0900

    Add font "Pretendard", theme.ts and ThemeProvider on App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { 
+  Stack,
+  ThemeProvider,
+  Typography
+} from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { theme } from "./theme";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        width="100%"
+        height="100%"
+        spacing="16px"
+      >
+        <Typography>Hello, world!</Typography>
+        <Typography>안녕, 세계!</Typography>
+        <Typography>"Icons8 Icons" http://icons8.com/icons</Typography>
+      </Stack>
+    </ThemeProvider>
+  );
 }
-
-export default App
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "Pretendard",
+    body1: {
+      fontSize: "16px",  
+      fontWeight: 100,
+    },
+  }
+});
